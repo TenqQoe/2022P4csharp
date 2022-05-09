2bd90f2899cb5ea5207d051ebcb5e7a61e57ca39
Tenq Boersma <33595@ma-web.nl>
2022-05-09 09:43:09 +0200

    initial commit

---
+++ opdracht 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace opdracht_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
